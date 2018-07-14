f6d73c46a9cf1574dc08f868294fc6e58786df1d
sameerkitkat <sameerkitkat4.4@gmail.com>
2018-07-14 19:59:27 +0530

    New Activity Addition

--- app/src/main/java/calculator/sameer/com/mycalc/MainActivity.java
+++ app/src/main/java/calculator/sameer/com/mycalc/MainActivity.java
@@ -1,5 +1,6 @@
 package calculator.sameer.com.mycalc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,17 +23,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        selectConversion();
+        ConvertLengthActivity();
+        ConvertAreaActivity();
     }
 
-    public void selectConversion(){
+/*    public void selectConversion(){
       // Find respective buttons in UI
       LengthButton = (Button) findViewById(R.id.Length_button);
       AreaButton   = (Button) findViewById(R.id.Area_button);
       MassButton   = (Button) findViewById(R.id.Mass_button);
     }
+*/
+    public void ConvertLengthActivity(){
+        final Context context = getBaseContext();
+        LengthButton = (Button) findViewById(R.id.Length_button);
+        LengthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context,Convert_Length_Activity.class);
+                startActivity(intent);
 
-    // onClick Methods for each button
+            }
+        });
+    }
+    public void ConvertAreaActivity() {
+        final Context context = getBaseContext();
+        LengthButton = (Button) findViewById(R.id.Area_button);
+        LengthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context,Convert_Area_Activity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+   /* // onClick Methods for each button
     public void ConvertLengthActivity(View view) {
         Log.d(LOG_TAG, "Convert Length Button clicked!");
         Intent intent = new Intent(this,Convert_Length_Activity.class);
@@ -41,11 +69,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void ConvertAreaActivity(View view) {
         Log.d(LOG_TAG, "Convert Area Button clicked!");
-
+        Intent intent = new Intent(this,Convert_Area_Activity.class);
+        startActivity(intent);
     }
 
     public void ConvertMassActivity(View view) {
         Log.d(LOG_TAG, "Convert Mass Button clicked!");
 
-    }
+    }*/
 }
